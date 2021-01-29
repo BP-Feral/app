288341d6b90df729ee8db0241c2674e54e921429
pricob <mihai.pricob@yahoo.com>
2021-01-30 01:42:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config();
 require("./server.js");
 
 const TOKEN = process.env.BOT_TOKEN;
-const PREFIX = process.env.PREFIX;
+const PREFIX = n;
 const GOOGLE_API_KEY = process.env.YTAPI_KEY;
 
 const bot = new Client({
